bd0070bef8296d5c30b12f8b9183bd0ab502cab7
Jananiviswa15 <73423520+Jananiviswa15@users.noreply.github.com>
2023-07-17 16:54:03 +0530

    Interfcae

---
+++ Interface.java
@@ -0,0 +1,15 @@
+package otherPackage;
+
+
+
+interface Interface{
+	//marker inter
+	//functional interf --> sam
+//u cant create obj and u cant have constr
+	
+	//abstract methods  void display();
+	
+	 void display(); //u dont need to preced with abst
+	 //def all methods in inter are public abs
+	 void sayHi();
+}
---
+++ InterfaceClass.java
@@ -0,0 +1,18 @@
+package otherPackage;
+
+public class InterfaceClass implements Interface{
+
+	public void display() {
+		
+	}
+	
+	public void sayHi() {
+		
+	}
+	
+}
+
+
+abstract class A implements Interface{
+	
+}
